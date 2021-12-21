18266a74c4ff3b2170138a21722279739e8eae6d
oscarcalsina24 <46852074+oscarcalsina24@users.noreply.github.com>
2021-12-21 12:54:00 -0500

    Update form.blade.php

--- resources/views/contactos/form.blade.php
+++ resources/views/contactos/form.blade.php
@@ -49,7 +49,7 @@
 
     <div class="col-md-6 col-12">
       <div class="form-label-group">
-        <input type="number" id="area" class="form-control" name="area" value="{{ old ( 'area', $contacto->dni ) }}"  placeholder="Area">
+        <input type="text" id="area" class="form-control" name="area" value="{{ old ( 'area', $contacto->dni ) }}"  placeholder="Area">
         <label for="dni">Area</label>
       </div>
     </div>
