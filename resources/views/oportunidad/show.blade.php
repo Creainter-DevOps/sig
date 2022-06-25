65abbd3f6d406e3b73b1e8efc1a80eda122027a8
Diego Anccas <diego.anccas@gmail.com>
2022-06-24 20:45:44 -0500

    m

--- resources/views/oportunidad/show.blade.php
+++ resources/views/oportunidad/show.blade.php
@@ -425,5 +425,70 @@
                 ])
             </div>
         </div>
+        <div>
+
+
+
+
+
+
+
+
+            <div class="card ">
+                <div class="card-header">
+                    <h4 class="card-title">
+                        Análisis de Oportunidad
+                    </h4>
+                </div>
+                <div class="card-content">
+                    <div class="card-body">
+                        <table class="table mb-0 table-sm" style="font-size:11px;">
+                            <thead>
+                                <tr>
+                                    <td>Servicio</th>
+                                    <td>Min</th>
+                                    <td>Prom.</td>
+                                    <th>Max.</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($oportunidad->similares() as $s)
+                                    <tr>
+                                        <td>
+                                            <div style="font-weight: bold;font-size: 9px;">{{ $s->licitaciones }} x
+                                                {{ $s->entidad }} - {{ $s->anho }}</div>
+                                            <div style="">{{ $s->rotulo }}</div>
+                                            <div>
+                                                @foreach (explode(',', $s->ids) as $l)
+                                                    <a href="/licitaciones/{{ $l }}/detalles"
+                                                        style="margin-right:5px;">{{ $l }}</a>
+                                                @endforeach
+                                            </div>
+                                        </td>
+                                        @if ($s->minimo == $s->maximo)
+                                            <td colspan="3" style="width:80px;font-size:11px;text-align:right;">
+                                                {{ Helper::money($s->minimo) }}</td>
+                                        @else
+                                            <td style="width:80px;font-size:11px;text-align:right;">
+                                                {{ Helper::money($s->minimo) }}</td>
+                                            <td style="width:80px;font-size:11px;text-align:right;">
+                                                {{ Helper::money($s->promedio) }}</td>
+                                            <td style="width:80px;font-size:11px;text-align:right;">
+                                                {{ Helper::money($s->maximo) }}</td>
+                                        @endif
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+          
+
+
+
+            
+        </div>
     </section>
 @endsection
